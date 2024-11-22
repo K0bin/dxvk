71fb2eaecaa6948b49445ea799744b9f57d96cc9
Robin Kertels <robin.kertels@gmail.com>
2024-11-23 00:09:29 +0100

    avoid vec3 conversion

--- src/dxso/dxso_srgb.h
+++ src/dxso/dxso_srgb.h
@@ -14,14 +14,12 @@ namespace dxvk {
   ) {
     uint32_t boolTypeId = module.defBoolType();
     uint32_t floatTypeId = module.defFloatType(32);
-    uint32_t vec3fTypeId = module.defVectorType(floatTypeId, 3);
-    uint32_t vec3bTypeId = module.defVectorType(boolTypeId, 3);
     uint32_t vec4fTypeId = module.defVectorType(floatTypeId, 3);
     uint32_t vec4bTypeId = module.defVectorType(boolTypeId, 4);
 
     uint32_t specConstVal = spec.get(module, specUbo, SpecSrgb, 0, 1);
     uint32_t doSrgb = module.opIEqual(boolTypeId, specConstVal, module.constu32(1u));
-    const std::array<uint32_t, 4> repeatSrgb = { doSrgb, doSrgb, doSrgb, doSrgb };
+    const std::array<uint32_t, 4> repeatSrgb = { doSrgb, doSrgb, doSrgb, module.constBool(false) /* Always get alpha from the original vector*/ };
     uint32_t doSrgbVec = module.opCompositeConstruct(vec4bTypeId, 4, repeatSrgb.data());
 
     for (uint32_t i = 0; i < caps::MaxSimultaneousRenderTargets; i++) {
@@ -29,26 +27,36 @@ namespace dxvk {
         // Shader doesn't write RT i
         continue;
 
-      uint32_t linearVal = module.opLoad(vec4fTypeId, rtRegIds[i]);
+      /*
+      Adapted from:
+      vec3 linear_to_srgb(vec3 linear) {
+        bvec3 isLo = lessThanEqual(linear, vec3(0.0031308f));
 
-      const std::array<uint32_t, 3> indices = { 0, 1, 2 };
-      uint32_t linearVec3 = module.opVectorShuffle(vec3fTypeId, linearVal, linearVal, 3, indices.data());
+        vec3 loPart = linear * 12.92f;
+        vec3 hiPart = pow(linear, vec3(5.0f / 12.0f)) * 1.055f - 0.055f;
+        return mix(hiPart, loPart, isLo);
+      }
+      We do however do all the math on vec4s to avoid having to convert it to vec3 and back.
+      At the end it will just take the alpha from the original RT register anyway.
+      */
 
-      const uint32_t lo = module.constvec3f32(0.0031308f, 0.0031308f, 0.0031308f);
-      uint32_t isLo = module.opFOrdLessThanEqual(vec3bTypeId, linearVec3, lo);
+      // Load linear render result
+      uint32_t linear = module.opLoad(vec4fTypeId, rtRegIds[i]);
 
-      uint32_t factorConstId = module.constvec3f32(12.92f, 12.92f, 12.92f);
-      uint32_t loPart = module.opFMul(vec3fTypeId, linearVec3, factorConstId);
+      const uint32_t lo = module.constvec4f32(0.0031308f, 0.0031308f, 0.0031308f, 0.0f);
+      uint32_t isLo = module.opFOrdLessThanEqual(vec4bTypeId, linear, lo);
 
-      uint32_t hiPart = module.opPow(vec3fTypeId, linearVec3, module.constvec3f32(5.0f / 12.0f, 5.0f / 12.0f, 5.0f / 12.0f));
-      hiPart = module.opFMul(vec3fTypeId, hiPart, module.constvec3f32(1.055f, 1.055f, 1.055f));
-      hiPart = module.opFSub(vec3fTypeId, hiPart, module.constvec3f32(0.055f, 0.055f, 0.055f));
+      uint32_t factorConstId = module.constvec4f32(12.92f, 12.92f, 12.92f, 1.0f);
+      uint32_t loPart = module.opFMul(vec4fTypeId, linear, factorConstId);
 
-      uint32_t srgbVec3 = module.opSelect(vec3fTypeId, isLo, loPart, hiPart);
-      const std::array<uint32_t, 4> finalVec4Indices = {0, 1, 2, 3};
-      uint32_t srgbVec4 = module.opVectorShuffle(vec4fTypeId, srgbVec3, linearVal, 4, finalVec4Indices.data());
+      uint32_t hiPart = module.opPow(vec4fTypeId, linear, module.constvec4f32(5.0f / 12.0f, 5.0f / 12.0f, 5.0f / 12.0f, 1.0f));
+      hiPart = module.opFMul(vec4fTypeId, hiPart, module.constvec4f32(1.055f, 1.055f, 1.055f, 1.0f));
+      hiPart = module.opFSub(vec4fTypeId, hiPart, module.constvec4f32(0.055f, 0.055f, 0.055f, 0.0f));
 
-      uint32_t finalRTOutput = module.opSelect(vec4fTypeId, doSrgbVec, srgbVec4, linearVal);
+      uint32_t srgb = module.opSelect(vec4fTypeId, isLo, loPart, hiPart);
+
+      // Decide whether we use the SRGB vector or the original linear one
+      uint32_t finalRTOutput = module.opSelect(vec4fTypeId, doSrgbVec, srgb, linear);
 
       module.opStore(rtRegIds[i], finalRTOutput);
     }
