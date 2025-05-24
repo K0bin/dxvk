0480c99c7be95a2ad1f032a9d1fe66ef6fc9f83a
WinterSnowfall <WinterSnowfall@users.noreply.github.com>
2025-05-24 10:04:40 +0300

    [d3d9] Properly reset some state vars on overall state reset

--- src/d3d9/d3d9_device.cpp
+++ src/d3d9/d3d9_device.cpp
@@ -8542,6 +8542,10 @@ namespace dxvk {
       SetStreamSource(i, nullptr, 0, 0);
     }
 
+    m_amdATOC          = false;
+    m_nvATOC           = false;
+    m_ffZTest          = false;
+
     m_clipPlaneEnabled = false;
     m_depthBiasEnabled = false;
     m_alphaTestEnabled = false;
